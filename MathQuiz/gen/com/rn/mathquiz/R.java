49aed91839582e700b334d40c8af63f6765a27d7
Ryan Nhieu <ryan.nhieu@gmail.com>
2014-04-07 23:06:59 -0400

    timer initial view

--- MathQuiz/gen/com/rn/mathquiz/R.java
+++ MathQuiz/gen/com/rn/mathquiz/R.java
@@ -24,19 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int answer=0x7f080004;
         public static final int combo=0x7f080007;
-        public static final int leaderboards=0x7f08000a;
+        public static final int leaderboards=0x7f08000b;
         public static final int number_1=0x7f080001;
         public static final int number_2=0x7f080003;
         public static final int operation=0x7f080002;
-        public static final int other=0x7f08000b;
+        public static final int other=0x7f08000c;
         public static final int question=0x7f080000;
         public static final int record=0x7f080006;
-        public static final int start=0x7f080009;
+        public static final int start=0x7f08000a;
         public static final int statistics=0x7f080005;
-        public static final int title=0x7f080008;
+        public static final int time=0x7f080008;
+        public static final int title=0x7f080009;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
